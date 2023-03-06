6dc22c1b91a5fa32fbca5e77d2f0fd6ffe02b71a
Guang Chen <kp82611@gmail.com>
2023-03-06 13:54:03 -0800

    update sign up

--- Startpage.py
+++ Startpage.py
@@ -1,8 +1,10 @@
 from flask import Flask, redirect, url_for, render_template, request
 import pymssql
+import re
 
-conn = pymssql.connect(host='your host', database='daba base name',
-                       user='user name', password='password', charset='utf8')
+
+conn = pymssql.connect(host='host', database='database',
+                       user='user', password='password', charset='utf8')
 cursor = conn.cursor()
 
 app = Flask(__name__)
@@ -15,6 +17,37 @@ def start():
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
+	msg = ''
+	if request.method == 'POST' and 'username' in request.form and 'firstname' in request.form and 'lastname' in request.form and 'dateofbirth' in request.form and 'ssn' in request.form and 'adderss' in request.form and 'gender' in request.form and 'email' in request.form:
+		username = request.form['username']
+		fname = request.form['firstname']
+		lname = request.form['lastname']
+		birth = request.form['dateofbirth']
+		ssn = request.form['ssn']
+		adderss = request.form['adderss']
+		gender = request.form['gender']
+		email = request.form['email']
+                
+		cursor.execute('SELECT * FROM [sample].[dbo].[user] WHERE [username] = %s', (username))
+		account = cursor.fetchone()
+		if account:
+			msg = 'Account already exists !'
+		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+			msg = 'Invalid email address !'
+		elif not re.match(r'[A-Za-z0-9]+', username):
+			msg = 'name must contain only characters and numbers !'
+		else:
+			cursor.execute('INSERT INTO [sample].[dbo].[user] VALUES (%s, %s, %s, %s, %s, %s, %s, %s);', (username, fname, lname, email, ssn, birth, adderss, gender))
+			msg = 'You have successfully registered !'
+	elif request.method == 'POST':
+		msg = 'Please fill out the form !'
+
+	return render_template("signup.html", msg = msg)
+
+
+
+@app.route("/login", methods =['GET', 'POST'])
+def login():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
@@ -29,12 +62,6 @@ def signup():
             msg = 'Incorrect username / password !'
 
     return render_template('login.html', msg=msg)
-
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
 
 @app.route("/why")
 def why():
